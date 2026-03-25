d06ef5aeef1122ffc26626a2f4c153694431f7c7
MohammadAliDimacaling <dimacalingmohammadali@gmail.com>
2026-03-25 16:32:32 +0800

    update env, index

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const userRoutes = require('./routes/user');
-const courseRoutes = require('./routes/course');
-const enrollmentRoutes = require('./routes/enrollment');
+const userRoutes = require("./routes/user");
+const courseRoutes = require("./routes/course");
+const enrollmentRoutes = require("./routes/enrollment");
 
 require('dotenv').config();
 const app = express();
 
-app.use(cors());
-
 mongoose.connect(process.env.MONGODB_STRING);
-
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () => console.log("We're connected to the cloud database"));
+let db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error"))
+db.once("open", () => console.log("We're connected to the cloud database"))
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 
-app.use('/users', userRoutes);
-app.use('/courses', courseRoutes);
-app.use('/enrollments', enrollmentRoutes);
+app.use(cors())
 
-if (require.main === module) {
-    app.listen(process.env.PORT || 3000, () => {
-        console.log(`API is now online on port ${process.env.PORT || 3000}`);
-    });
+const corsOptions = {
+	origin: ['http://localhost:5173'],
+	credentials: true, 
+	optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.use("/users", userRoutes);
+app.use("/courses", courseRoutes);
+app.use("/enrollments", enrollmentRoutes);
+
+
+if(require.main === module){
+    app.listen(process.env.PORT || 3000, () => 
+    	console.log(`API is now online on port ${process.env.PORT || 3000}`));
 }
 
-module.exports = { app, mongoose };
+module.exports = {app,mongoose};
